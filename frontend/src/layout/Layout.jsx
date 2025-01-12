7bbadfba8524f30cf68dfda870e307017a70e0d9
Kanishka Gunasinghe <kanishkapg1121@gmail.com>
2025-01-12 19:44:52 +0530

    Routers are set

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
-import Header from './Header'
-import Footer from './Footer'
+import Header from '../components/header/Header.jsx'
+import Footer from '../components/footer/Footer.jsx'
 
 const Layout = ({children}) => { 
   // children is the child components that will be rendered
