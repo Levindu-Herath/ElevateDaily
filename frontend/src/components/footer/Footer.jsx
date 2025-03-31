f820475bcea4be2b7da3ec7786dfbf69182431ef
Levindu Herath <levinduherath@gmail.com>
2025-03-31 16:00:29 +0530

    footer change with health

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <li><Link to="posts/categories/Business">Business</Link></li>
         <li><Link to="posts/categories/Education">Education</Link></li>
         <li><Link to="posts/categories/Entertainment">Entertainment</Link></li>
-        <li><Link to="posts/categories/Art">Art</Link></li>
+        <li><Link to="posts/categories/Art">Health</Link></li>
         <li><Link to="posts/categories/Investment">Investment</Link></li>
         <li><Link to="posts/categories/Weather">Weather</Link></li>
         <li><Link to="posts/categories/Uncategorized">Uncategorized</Link></li>
